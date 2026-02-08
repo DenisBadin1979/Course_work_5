e7c88699b0ab598ac7b3647ea3513e352884e55b
Denis_Badin <denisbadin@yandex.ru>
2026-02-08 22:54:04 +0500

    начало работы с моделями для привчек, место, вознаграждения

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit, Place, Award
+
+admin.site.register(Habit)
+admin.site.register(Place)
+admin.site.register(Award)
--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,28 @@
+from symtable import Class
+
 from django.db import models
 
-# Create your models here.
+class Habit (models.Model):
+
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+    pass
+
+class Place (models.Model):
+
+
+    class Meta:
+        verbose_name = "Место действия"
+        verbose_name_plural = "Места действия"
+
+    pass
+
+class Award (models.Model):
+
+    class Meta:
+        verbose_name = "Вознаграждение"
+        verbose_name_plural = "Вознаграждения"
+
+    pass
